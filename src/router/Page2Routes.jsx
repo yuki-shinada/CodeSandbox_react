e84e22c1c769f5838532b7e2c6cfa4148fa22ab9
yuki-shinada <132135382+yuki-shinada@users.noreply.github.com>
2023-04-29 19:01:15 +0900

    Initial commit

---
+++ src/router/Page2Routes.jsx
@@ -0,0 +1,15 @@
+import { Page2 } from "../Page2";
+import { UrlParameter } from "../UrlParameter";
+
+export const Page2Routes = [
+  {
+    path: "/",
+    exact: true,
+    children: <Page2 />
+  },
+  {
+    path: "/:id",
+    exact: false,
+    children: <UrlParameter />
+  }
+];
---
+++ src/router/Router.jsx
@@ -0,0 +1,47 @@
+import { Switch, Route } from "react-router-dom";
+
+import { Home } from "../Home";
+import { Page1Routes } from "./Page1Routes";
+import { Page2Routes } from "./Page2Routes";
+
+export const Router = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route
+        path="/page1"
+        render={({ match: { url } }) => (
+          <Switch>
+            {Page1Routes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      />
+      <Route
+        path="/page2"
+        render={({ match: { url } }) => (
+          <Switch>
+            {Page2Routes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      />
+    </Switch>
+  );
+};
